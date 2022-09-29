7a0336aae916461e14ca07b810e70f5aae27a37d
lcapuano <lzuchetti@gmail.com>
2022-09-29 13:54:55 -0300

    check if .env file exists in project, then in workspace. Raise on fail

--- src/main.py
+++ src/main.py
@@ -6,10 +6,23 @@ from datetime import datetime
 from dotenv import load_dotenv
 
 from imserver import im_server
-from definitions import LOGS_DIR, ROOT_DIR
+from definitions import LOGS_DIR, ROOT_DIR, WORKSPACE_DIR
 
-dotenv_path = os.path.join(ROOT_DIR, '.env')
-load_dotenv(dotenv_path)
+def _dot_env():
+    dotenv_root_path = os.path.join(ROOT_DIR, '.env')
+    dotenv_ws_path = os.path.join(WORKSPACE_DIR, '.env')
+
+    if os.path.exists( dotenv_root_path ):
+        load_dotenv(dotenv_root_path)
+    elif os.path.exists( dotenv_ws_path ):
+        load_dotenv(dotenv_ws_path)
+    else:
+        raise FileExistsError(f'{Fore.LIGHTRED_EX}Could not found .env file.{Style.RESET_ALL}')
+
+def _print_wellcome():
+    print(Fore.GREEN)
+    print(pyfiglet.figlet_format( "imgen" ))
+    print(Style.RESET_ALL)
 
 def _load_logger() -> None:
 
@@ -28,9 +41,8 @@ def _load_logger() -> None:
 
 
 def main():
-    print(Fore.GREEN)
-    print(pyfiglet.figlet_format( "imgen" ))
-    print(Style.RESET_ALL)
+    _print_wellcome()
+    _dot_env()
     _load_logger()
     im_server.init()
 
